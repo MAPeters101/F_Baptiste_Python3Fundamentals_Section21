311e7bda3779fc434fbc60794575026842920674
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:21:23 -0400

    Description for exercise01.

---
+++ exercise01.py
@@ -0,0 +1,9 @@
+'''
+Question 1
+Write a function that, given an epoch timestamp, returns a datetime object set
+to the beginning of that month (so midnight of the first day of the month).
+
+For example, given the epoch time 12345678.9, your function should return:
+
+datetime.datetime(1970, 5, 1, 0, 0)
+'''
