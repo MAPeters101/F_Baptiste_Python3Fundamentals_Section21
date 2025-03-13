e2bb344d344dc9d31c17ccc01facdb85018e8a7b
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:24:42 -0400

    Description for exercise03.

---
+++ exercise03.py
@@ -0,0 +1,10 @@
+'''
+Question 3
+Write a function that can be used to consistently format datetime objects into
+strings with some default format, but allows the caller to override the
+default format.
+
+For example, the default format could be mm/dd/yyyy hh:mm:ss am/pm, but your
+function allows itself to be called with some argument that can override that
+format.
+'''
