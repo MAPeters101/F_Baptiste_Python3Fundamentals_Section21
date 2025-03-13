a0f63a6c24f0da2f3a86515dcd0fee6f6406ae02
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:27:37 -0400

    Reformat descriptions for solutions.

--- solutions.py
+++ solutions.py
@@ -1,6 +1,7 @@
 '''
 Question 1
-Write a function that, given an epoch timestamp, returns a datetime object set to the beginning of that month (so midnight of the first day of the month).
+Write a function that, given an epoch timestamp, returns a datetime object set
+to the beginning of that month (so midnight of the first day of the month).
 
 For example, given the epoch time 12345678.9, your function should return:
 
@@ -9,12 +10,15 @@ Solution
 First we'll need to import the datetime object:
 
 from datetime import datetime
-We can easily convert an epoch timestamp to a datetime object by using the fromtimestamp method.
+We can easily convert an epoch timestamp to a datetime object by using the
+fromtimestamp method.
 
 dt = datetime.fromtimestamp(12345678.9)
 dt
 datetime.datetime(1970, 5, 23, 14, 21, 18, 900000)
-Next, we could replace the day, hour, minutes, and seconds with 1 or 0 as needed, or we could create a new datetime object just picking up the year and month from this one:
+Next, we could replace the day, hour, minutes, and seconds with 1 or 0 as
+needed, or we could create a new datetime object just picking up the year and
+month from this one:
 
 dt.replace(day=1, hour=0, minute=0, second=0, microsecond=0)
 datetime.datetime(1970, 5, 1, 0, 0)
@@ -37,7 +41,9 @@ import time
 month_start(time.time())
 datetime.datetime(2021, 2, 1, 0, 0)
 Question 2
-Write a function that returns the difference in hours between two dates provided as Python standard ISO formatted strings, rounded to the nearest hour. For simplicity, assume that these dates do not contain fractional seconds.
+Write a function that returns the difference in hours between two dates
+provided as Python standard ISO formatted strings, rounded to the nearest hour.
+For simplicity, assume that these dates do not contain fractional seconds.
 
 For example, given these two dates:
 
@@ -68,9 +74,11 @@ An hour is 60 * 60 seconds:
 seconds_in_hour = 60 * 60
 So, we want to round to the closest multiple of seconds_in_hour.
 
-There are probably many different approaches to this, here I'll explain how I thought about the problem.
+There are probably many different approaches to this, here I'll explain how I
+thought about the problem.
 
-First thing is we'll want to make the total number of seconds into an integer (we are assuming no fractional seconds).
+First thing is we'll want to make the total number of seconds into an integer
+(we are assuming no fractional seconds).
 
 delta_seconds = int(delta.total_seconds())
 Next, we'll calculate the number of whole hours in that:
@@ -87,7 +95,8 @@ The number of (fractional) hours in these seconds is:
 
 remaining_seconds / seconds_in_hour
 0.5575
-We can simply round this number to determine whether we are closer to 0 or 1 for the farctional hour:
+We can simply round this number to determine whether we are closer to 0 or 1
+for the farctional hour:
 
 round(remaining_seconds / seconds_in_hour)
 1
@@ -110,18 +119,25 @@ num_hours('2001-01-01T00:00:00', '2001-01-01T01:00:00')
 num_hours('2001-01-01T00:00:00', '2001-01-02T01:50:00')
 26
 Question 3
-Write a function that can be used to consistently format datetime objects into strings with some default format, but allows the caller to override the default format.
+Write a function that can be used to consistently format datetime objects into
+strings with some default format, but allows the caller to override the
+default format.
 
-For example, the default format could be mm/dd/yyyy hh:mm:ss am/pm, but your function allows itself to be called with some argument that can override that format.
+For example, the default format could be mm/dd/yyyy hh:mm:ss am/pm, but your
+function allows itself to be called with some argument that can override that
+format.
 
 Solution
 Let's write the function definition first:
 
 def dt_to_string(dt, fmt='%m/%d/%y %I:%M:%S%p'):
     pass
-Notice how I set the default format string in the parameter definition itself - this way if the function is called without that argument, then the default will be used.
+Notice how I set the default format string in the parameter definition itself -
+this way if the function is called without that argument, then the default
+will be used.
 
-For these types of parameters though, I prefer to force the function to specify that fmt argument as a keyword argument, so I can force that this way:
+For these types of parameters though, I prefer to force the function to
+specify that fmt argument as a keyword argument, so I can force that this way:
 
 def dt_to_string(dt, *, fmt='%m/%d/%y %I:%M:%S%p'):
     pass
