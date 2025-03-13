3992e886d0bc139f9348d8c5c5df90e88843fdef
Mark Peters <mapeters101@earthlink.net>
2025-03-12 23:23:10 -0400

    Description for exercise02.

---
+++ exercise02.py
@@ -0,0 +1,14 @@
+'''
+Question 2
+Write a function that returns the difference in hours between two dates
+provided as Python standard ISO formatted strings, rounded to the nearest hour.
+For simplicity, assume that these dates do not contain fractional seconds.
+
+For example, given these two dates:
+
+2001-01-01T13:50:23
+and
+
+2001-06-12T14:23:50
+your result should be 3889 hours.
+'''
